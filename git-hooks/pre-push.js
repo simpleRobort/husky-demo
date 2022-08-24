2fcfa991a2d17c69156319638d9869d17d6346da
ByteDance <zhangzeyang.27@bytedance.com>
2022-08-24 16:08:41 +0800

    6

---
+++ git-hooks/pre-push.js
@@ -0,0 +1,9 @@
+// const { execSync } = require('child_process');
+// const path = require('path');
+// const fs = require('fs');
+
+console.log("push之前")
+console.log(`无权限修改公共组件！1`);
+// process.exit(1)
+
+console.log("无权限修改公共组件！")
